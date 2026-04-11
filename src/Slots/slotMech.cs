c61dc1e6b23182e0dfbab78fb5962451bb19f804
Jonas Kvist <96206419+SilentLord23@users.noreply.github.com>
2026-04-11 14:01:42 +0200

    Added initialisation of slotmech and generation of 2d array

--- src/Slots/slotMech.cs
+++ src/Slots/slotMech.cs
@@ -7,13 +7,25 @@ public class slotMech
     public slotMech(int rows, int columns)
     {
         // TODO: Initialize the grid dimensions and the Random object
+        this.rows = rows;
+        this.columns = columns;
+        rng = new Random();
     }
 
     public int[,] Spin(int totalSymbols)
     {
         // TODO: Create a 2D array based on rows and columns
+        int[,] grid = new int[rows, columns];
         // TODO: Fill each index of the array by calling GetRandomSymbol
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                grid[i, j] = GetRandomSymbol(totalSymbols);
+            }
+        }
         // TODO: Return the completed matrix to the GameManager heheheha
+        return grid;
     }
 
     public void SetStopLogic()
@@ -25,5 +37,6 @@ public class slotMech
     private int GetRandomSymbol(int totalSymbols)
     {
         // TODO: Return a random integer between 0 and the total number of symbols
+        return rng.Next(0, totalSymbols);
     }
 }
